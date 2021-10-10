ae5511e44e032e4cc38a1c0df8a2fb63c9ab4bfa
sandysheridan <91229506+sandysheridan@users.noreply.github.com>
2021-10-09 23:47:13 -0400

    Update CardTrick.java
    Printing card value and suit

--- src/cardtrick/CardTrick.java
+++ src/cardtrick/CardTrick.java
@@ -14,6 +14,8 @@ public class CardTrick {
     public static void main(String[] args)
     {
        Card card = new Card();
+       System.out.println(card.getSuit());
+       System.out.println(card.getValue());
     }
     
 }
