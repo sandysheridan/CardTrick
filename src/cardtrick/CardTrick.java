bf14822308ac06209d6d97262b9b56bb638db976
Sandy <sus@sheridancollege.ca>
2021-10-09 23:52:09 -0400

    Set value and suit, created card

--- src/cardtrick/CardTrick.java
+++ src/cardtrick/CardTrick.java
@@ -14,6 +14,8 @@ public class CardTrick {
     public static void main(String[] args)
     {
        Card card = new Card();
+       card.setValue(8);
+       card.setSuit("Hearts");
        System.out.println(card.getSuit());
        System.out.println(card.getValue());
     }
